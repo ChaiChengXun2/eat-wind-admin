12c1ac9732950959912eae002f745c135f91d2d5
Chai Cheng Xun <chaibicai@gmail.com>
2022-12-01 21:18:34 +0800

    added minor detail

--- src/LoggedIn/CreateModal/OptionalData/index.js
+++ src/LoggedIn/CreateModal/OptionalData/index.js
@@ -1,17 +1,20 @@
 import React, { useState } from 'react';
 import "./optionaldata.css";
+import "./responsive.css";
 import { AiFillDelete } from "react-icons/ai";
 
 const OptionalData = ({ setNumber, setOpt }) => {
 
   const [data, setData] = useState("");
+  const [word, setWord] = useState("Add");
 
   return (
     <section className='flex-center-between optional-data optional-des'>
       <input className='default-text other' placeholder='' autoComplete='off' type="text" onChange={e => setData(e.target.value)}></input>
-      <div className='default-button' onClick={() => {
-        setOpt(prev => [...prev, data])
-      }}>Done</div>
+      <div className='default-button done-button' onClick={(e) => {
+        setOpt(prev => [...prev, data]);
+        setWord("Added");
+      }}>{word}</div>
       <div onClick={() => setNumber(prev => prev - 1)}><AiFillDelete /></div>
     </section>
   )
